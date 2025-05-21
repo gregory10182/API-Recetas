de04e9fafda7fff25fb5dc6543ab747ef52dbf40
Gregory Perez <gregory10183@gmail.com>
2025-05-21 11:10:34 -0400

    removing code that didnt work

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 import app from "./app.js";
-import fs from "fs";
-import https from "https";
 import { connect } from "./utils/mongoose.js";
-
-const key = fs.readFileSync("./ssl/private.key");
-const cert = fs.readFileSync("./ssl/certificate.crt");
-
-const credentials = { key, cert };
 
 async function main() {
   await connect();
+
   app.listen(8000, () => {
     console.log("Server on port 8000");
   });
-
-  https.createServer(credentials, app).listen(443, () => {
-    console.log("Server on port 443");
-  })
 }
 
 main();
